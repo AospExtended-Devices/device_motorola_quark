c6738bc528a31b2fd1e826301f70ba7b4e488f45
Hlcpereira <pereirah2001@gmail.com>
2018-04-16 01:41:49 +0000

    quark: AEX bringup

--- aosp_quark.mk
+++ aosp_quark.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/motorola/quark/full_quark.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := Moto MAXX
-PRODUCT_NAME := lineage_quark
+PRODUCT_NAME := aosp_quark
 
 #needed for play
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
