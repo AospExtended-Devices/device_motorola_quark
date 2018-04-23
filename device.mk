f4ebe9444414d20504d7b97b6010822566cdb60b
Hlcpereira <pereirah2001@gmail.com>
2018-04-23 01:53:23 +0000

    Fix derps * Rename Snap overlay to SnapdragonCamera * Fix bootloop * Any others that I forgot

--- device.mk
+++ device.mk
@@ -67,7 +67,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
 
-#$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 #$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 # Audio
